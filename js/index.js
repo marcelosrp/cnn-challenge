a03761aae36900b1febcd53adcfa94afcff37260
Marcelo Soares <soares.srp@gmail.com>
2022-03-18 16:27:37 -0300

    Base inicial

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 import '../scss/main.scss'
+
+/* const URL_API = 'https://alpha.cnnbrasil.com.br/wp-json/wp/v2/recruitment-posts'
+
+async function getApiPosts() {
+  const response = await fetch(URL_API)
+  console.log(response)
+
+  const data = await response.json()
+
+  console.log(data)
+}
+
+getApiPosts() */

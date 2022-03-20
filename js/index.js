fa2f03def790baf3811edad57bf6f0207674d790
Marcelo Soares <soares.srp@gmail.com>
2022-03-19 22:09:56 -0300

    parcial fetch api posts

--- js/index.js
+++ js/index.js
@@ -1,12 +1,61 @@
-/* const URL_API = 'https://alpha.cnnbrasil.com.br/wp-json/wp/v2/recruitment-posts'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+const URL_API = 'https://alpha.cnnbrasil.com.br/wp-json/wp/v2/recruitment-posts'
+
+const postTemplate = document.querySelector('#post-template')
+const postList = document.querySelector("[data-js='posts-list']")
 
 async function getApiPosts() {
-  const response = await fetch(URL_API)
-  console.log(response)
+  try {
+    const response = await fetch(URL_API)
 
-  const data = await response.json()
+    if (!response.ok) {
+      throw new Error('Um erro foi encontrado, por favor atualize a página')
+    }
 
-  console.log(data)
+    const data = await response.json()
+
+    showPosts(data)
+  } catch (error) {
+    console.log('Error: ', error)
+  }
 }
 
-getApiPosts() */
+function showPosts(posts) {
+  Object.entries(posts).forEach((post) => {
+    postTemplate.content.querySelector('.post-title').innerHTML =
+      post[1].post_title
+
+    postTemplate.content.querySelector('.post-resume').innerHTML =
+      post[1].post_excerpt
+
+    post[1].post_authors.forEach((author) => {
+      postTemplate.content.querySelector('.post-author__avatar').src =
+        author.author_img.thumbnail[0]
+
+      postTemplate.content.querySelector('.post-author__avatar').alt =
+        author.author_name
+
+      postTemplate.content.querySelector('.post-author__name').innerHTML =
+        author.author_name
+    })
+
+    postTemplate.content.querySelector('.post-author__post-date').innerHTML =
+      moment(post[1].post_modified).format('LLL')
+
+    if (post[1].featured_images !== null) {
+      postTemplate.content.querySelector('.post-image').src =
+        post[1].featured_images.full
+
+      postTemplate.content.querySelector('.post-image').alt = post[1].post_title
+    }
+
+    postTemplate.content.querySelector('.post-button').href = post[1].guid
+
+    const clone = document.importNode(postTemplate.content, true)
+    postList.appendChild(clone)
+  })
+}
+
+getApiPosts()

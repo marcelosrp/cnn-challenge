49f90b0e275a68f7d54ea2029b7497de88a79ace
Marcelo Soares <soares.srp@gmail.com>
2022-03-22 16:25:17 -0300

    carregando mais posts

--- fronted/js/get_posts.js
+++ fronted/js/get_posts.js
@@ -2,10 +2,16 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 import { URL_API } from './api'
 
+const buttonMaisPosts = document.querySelector("[data-js='button-mais-posts']")
+
 const Posts = {
   postTemplate: document.querySelector('#post-template'),
   postList: document.querySelector("[data-js='posts-list']"),
   loadingSpinner: document.querySelector("[data-js='loading']"),
+
+  init() {
+    this.getApiPosts()
+  },
 
   async getApiPosts() {
     try {
@@ -27,27 +33,30 @@ const Posts = {
       this.postList.innerHTML = errorTemplate
     } finally {
       this.loadingSpinner.remove()
+      buttonMaisPosts.classList.remove('d-none')
     }
   },
 
   showPosts(posts) {
-    Object.entries(posts).forEach((post) => {
+    const postsArray = Object.values(posts)
+
+    postsArray.forEach((post) => {
       this.postTemplate.content.querySelector(
         "[data-js='post-title']"
-      ).innerHTML = post[1].post_title
+      ).innerHTML = post.post_title
 
       this.postTemplate.content.querySelector(
         "[data-js='post-resume']"
-      ).innerHTML = post[1].post_excerpt
+      ).innerHTML = post.post_excerpt
 
       this.postTemplate.content.querySelector(
         "[data-js='post-publish-data']"
-      ).innerHTML = moment(post[1].post_modified).format('LLL')
+      ).innerHTML = moment(post.post_modified).format('LLL')
 
       this.postTemplate.content.querySelector("[data-js='post-button']").href =
-        post[1].guid
+        post.guid
 
-      post[1].post_authors.forEach((author) => {
+      post.post_authors.forEach((author) => {
         const postAuthorAvatar = this.postTemplate.content.querySelector(
           "[data-js='post-author-avatar']"
         )
@@ -61,19 +70,58 @@ const Posts = {
         postAuthorName.innerHTML = author.author_name
       })
 
-      if (post[1].featured_images !== null) {
+      if (post.featured_images !== null) {
         const postImage = this.postTemplate.content.querySelector(
           "[data-js='post-image']"
         )
 
-        postImage.src = post[1].featured_images.full
-        postImage.alt = post[1].post_title
+        postImage.src = post.featured_images.full
+        postImage.alt = post.post_title
       }
 
       const clone = document.importNode(this.postTemplate.content, true)
       this.postList.appendChild(clone)
     })
+
+    this.filtraPosts()
+  },
+
+  filtraPosts() {
+    const posts = Array.from(
+      document.querySelectorAll('[data-js="content-post"]')
+    )
+
+    const limit = 2
+
+    posts.forEach((item, index) => {
+      if (index > limit - 1) {
+        item.classList.add('d-none')
+      }
+    })
+  },
+
+  loadMorePosts() {
+    const posts = document.querySelectorAll('.posts-content__post.d-none')
+    const loadPosts = 2
+
+    this.postList.scrollIntoView({ block: 'end', behavior: 'smooth' })
+
+    posts.forEach((item, index) => {
+      if (index < loadPosts) {
+        item.classList.remove('d-none')
+      }
+
+      if (
+        document.querySelectorAll('.posts-content__post.d-none').length === 0
+      ) {
+        buttonMaisPosts.style.display = 'none'
+      }
+    })
   }
 }
 
-Posts.getApiPosts()
+Posts.init()
+
+buttonMaisPosts.addEventListener('click', () => {
+  Posts.loadMorePosts()
+})
